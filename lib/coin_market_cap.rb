69236cc072cf600365935b15eb421f82c5632567
Enow Mbi <benowmbi@yahoo.com>
2022-09-26 15:24:18 +0100

    Autoload exceptions module

--- lib/coin_market_cap.rb
+++ lib/coin_market_cap.rb
@@ -2,6 +2,13 @@
 
 require_relative "coin_market_cap/version"
 
+# CoinMarketCap
 module CoinMarketCap
-  # Your code goes here...
+  autoload(:Cryptocurrency, "coin_market_cap/cryptocurrency")
+  autoload(:BadRequestException, "coin_market_cap/exceptions")
+  autoload(:UnauthorizedException, "coin_market_cap/exceptions")
+  autoload(:PaymentRequiredException, "coin_market_cap/exceptions")
+  autoload(:ForbiddenException, "coin_market_cap/exceptions")
+  autoload(:TooManyRequestsException, "coin_market_cap/exceptions")
+  autoload(:ServerException, "coin_market_cap/exceptions")
 end
