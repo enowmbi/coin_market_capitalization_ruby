ab8bb7141048f89139ea26b4f7c6f7e24584facf
Enow Mbi <benowmbi@yahoo.com>
2022-09-27 03:29:27 +0100

    Add fiat class with methods mapped to fiat endpoints

---
+++ lib/coin_market_cap/fiat.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+module CoinMarketCap
+  # Fiat class contains methods mapped to corresponding API endpoints for fiat currencies.
+  # This category currently includes 1 endpoint
+  class Fiat < Base
+    BASE_URL = "https://pro-api.coinmarketcap.com/v1/fiat"
+
+    def initialize(api_key:, base_url: BASE_URL, adapter: Faraday.default_adapter)
+      super
+    end
+
+    def map(**args)
+      make_request("map", **args)
+    end
+  end
+end
