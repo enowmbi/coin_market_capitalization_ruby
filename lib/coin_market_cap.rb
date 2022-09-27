21c445d4fda60c4cfc052e040e0dadcec31bc3d4
Enow Mbi <benowmbi@yahoo.com>
2022-09-27 08:54:00 +0100

    Autoload fiat,exchange,global_metrics and blockchain

--- lib/coin_market_cap.rb
+++ lib/coin_market_cap.rb
@@ -6,6 +6,10 @@ require_relative "coin_market_cap/version"
 module CoinMarketCap
   autoload(:Base, "coin_market_cap/base")
   autoload(:Cryptocurrency, "coin_market_cap/cryptocurrency")
+  autoload(:Fiat, "coin_market_cap/fiat")
+  autoload(:Exchange, "coin_market_cap/exchange")
+  autoload(:GlobalMetrics, "coin_market_cap/global_metrics")
+  autoload(:Blockchain, "coin_market_cap/blockchain")
   autoload(:BadRequestException, "coin_market_cap/exceptions")
   autoload(:UnauthorizedException, "coin_market_cap/exceptions")
   autoload(:PaymentRequiredException, "coin_market_cap/exceptions")
